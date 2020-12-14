da42be3680f63daca965bed151737427bd10085e
Antoine <antekurk@gmamil.com>
2020-12-14 20:15:00 +0100

    Interface finie WPF

--- Client_WPF/MainWindow.xaml.cs
+++ Client_WPF/MainWindow.xaml.cs
@@ -17,77 +17,21 @@ using System.Net.Sockets;
 
 namespace Client_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            Console.OutputEncoding = Encoding.UTF8;
+            string test = "Sauvegarde 1";
+            string test2 = "Sauvegarde 2";
+            string test3 = "Sauvegarde 3";
+            string test4 = "Sauvegarde 4";
 
-            byte[] data = new byte[1024];
-
-            string input, stringData;
-
-            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
-
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            try
-            {
-                server.Connect(ipep);
-
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("Unable to connect to server.");
-
-                Console.WriteLine(e.ToString());
-
-                return;
-            }
-
-            int recv = server.Receive(data);
-
-            stringData = Encoding.UTF8.GetString(data, 0, recv);
-
-            Console.WriteLine(stringData);
-
-            while (true)
-            {
-                input = Console.ReadLine();
-
-                if (input == "exit")
-
-                    break;
-
-                Console.WriteLine("Vous: " + input);
-
-                server.Send(Encoding.UTF8.GetBytes(input));
-
-                data = new byte[1024];
-
-                recv = server.Receive(data);
-
-                stringData = Encoding.UTF8.GetString(data, 0, recv);
-
-                byte[] utf8string = System.Text.Encoding.UTF8.GetBytes(stringData);
-
-                Console.WriteLine("Server: " + stringData);
-            }
-
-            Console.WriteLine("Disconnecting from server...");
-
-            server.Shutdown(SocketShutdown.Both);
-
-            server.Close();
-
-            Console.WriteLine("Disconnected!");
-
-            Console.ReadLine();
+            SavesList.Items.Add(test);
+            SavesList.Items.Add(test2);
+            SavesList.Items.Add(test3);
+            SavesList.Items.Add(test4);
         }
     }
 }
